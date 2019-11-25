b6a593f2f742eda1643778ca7df3fd91958e7313
daniel.alves <daniel.alves@evoluservices.com>
2019-11-25 11:03:11 -0300

    Adiciona configuração de Linting.
    Signed-off-by: daniel.alves <daniel.alves@evoluservices.com>

--- core/src/index.js
+++ core/src/index.js
@@ -1,5 +1,7 @@
 import './index.css'
 import numeral from 'numeral';
 
+/* eslint-disable no-console */
+
 const courseValue = numeral(1000).format('R$0,00');
 console.log(`Eu gostaria de pagar ${courseValue} para fazer esse curso.`)
